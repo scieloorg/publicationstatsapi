0fe0c6f0b8675c2bebcfe71e9c0a2c309ed084cf
Gustavo Fonseca <gustavo@gfonseca.net>
2018-10-17 15:34:12 -0300

    Ajusta os dados de distribuição

--- setup.py
+++ setup.py
@@ -2,30 +2,25 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'thriftpy>=0.3.1',
-    'requests>=2.11.1'
+    'thriftpy',
+    'requests'
 ]
-
-tests_require = []
 
 setup(
     name="publicationstatsapi",
     version="1.1.0",
-    description="Library that implements the endpoints of the Access Stats API",
+    description="SciELO PublicationStats service SDK for Python",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
-    maintainer="Fabio Batalha",
-    maintainer_email="fabio.batalha@scielo.org",
-    url="http://github.com/scieloorg/processing",
-    packages=find_packages(),
+    url="http://github.com/scieloorg/publicationstatsapi",
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
     ],
-    dependency_links=[],
-    tests_require=tests_require+install_requires,
     test_suite='tests',
     install_requires=install_requires
 )
