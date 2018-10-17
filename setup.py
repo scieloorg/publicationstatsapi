118995d75ef9478f64d5707107f4ce1640a0aa8e
Gustavo Fonseca <gustavo@gfonseca.net>
2018-10-17 15:34:47 -0300

    Incrementa a versão do release
    O incremento foi realizado na *minor version* para compatibilizar com a
    versão disponível no PyPi.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 
 setup(
     name="publicationstatsapi",
-    version="1.1.0",
+    version="1.2.1",
     description="SciELO PublicationStats service SDK for Python",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
